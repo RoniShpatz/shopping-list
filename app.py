48e5dae27fd04120b9a4a21d29b3c30de3cf0765
ShpatzR <roni1kohen@gmail.com>
2024-08-12 20:18:26 +0100

    start authorization

--- app.py
+++ app.py
@@ -1,23 +1,87 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
-
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+import os
+import psycopg2
+from psycopg2 import sql
 from flask_bcrypt import Bcrypt 
+# making an env var 
+from dotenv import load_dotenv  # take environment variables from .env.
 
 app = Flask(__name__) 
-secret_key = "my_secret_key"
+
 bcrypt = Bcrypt(app)
-
-
-
+# protect my info  
+load_dotenv()
+username = os.getenv('USER_NAME')
+db_password = os.getenv('USER_PASSWORD')
+secret_key = os.getenv('SECRET_KEY')
 
 app.secret_key = secret_key
 
+def hash_password(password):
+    return bcrypt.generate_password_hash(password).decode('utf-8')
+
+
+def get_db_connection():
+    try:
+        connection = psycopg2.connect(
+            host="localhost",
+            port=5433,
+            database="shoppingList",
+            user=username,
+            password=db_password
+        )
+        return connection
+    except psycopg2.OperationalError as e:
+        print(f"Error: {e}")
+        return None
+
+def  verify_password(hashed_password, user_password):
+    return bcrypt.check_password_hash(hashed_password, user_password)
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return redirect("login")
+        return redirect(url_for('login'))
+            
+
    
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        connection = get_db_connection()
+        if connection:
+            cur = connection.cursor()
+            try:
+                cur.execute('SELECT password FROM users WHERE username = %s', (username,))
+                result = cur.fetchone()
+                print(result)
+                if result:
+                    stored_hash = result[0]
+                    if verify_password(stored_hash, password):
+                        session['username'] = username
+                        flash('Login successful')
+                        return redirect(url_for('home'))
+                    else:
+                        flash('Invalid password')
+                        return render_template("login.html", NoAuthorize=True)
+                else:
+                    flash('Username not found')
+                    return render_template("login.html", NoUsername=True)
+            except psycopg2.DatabaseError as e:
+                print(f"Error: {e}")
+                return render_template("login.html")
+            finally:
+                cur.close()
+                connection.close()
+        else:
+            flash('Database connection error')
+            return render_template("login.html")
+    
     return render_template("login.html")
+
+   
 
 
 @app.route("/sign-in", methods=["POST", "GET"])
@@ -26,13 +90,41 @@ def signin():
         name = request.form.get("username")
         password = request.form.get("password")
         password2 = request.form.get("password-2")
-        color = request.form.get("color")
-        isUniq = False
-        if password == password2 and color and isUniq == False:
-            session['username'] = name
-            session['profile'] = name[0]
-            session["color"] = color
-            return redirect(url_for('home'))
+        # color = request.form.get("color")
+        # print(name)
+        if password == password2:
+            connection = get_db_connection()
+            if connection:
+                    cur = connection.cursor()
+                    cur.execute('SELECT * FROM users WHERE username = %s', (name,))
+                    connection.commit()
+                    user = cur.fetchone()
+                    if user:
+                        cur.close()
+                        connection.close()
+                        return render_template('sign-in.html', password2_error=False, name= True)
+                    try:
+                        hashed_password = hash_password(password)
+                        cur.execute(
+                            'INSERT INTO users (username, password) VALUES (%s, %s)',
+                            (name, hashed_password)
+                        )
+                        connection.commit()
+                        cur.close()
+                        connection.close()
+                        session['username'] = name
+                        session['profile'] = name[0]
+                        # session["color"] = color
+                        return redirect(url_for('home'))
+                    except psycopg2.IntegrityError as e:
+                        print(f"Error inserting user: {e}")
+                        flash("An error occurred while saving the user.")
+                        cur.close()
+                        connection.close()
+                        return render_template('sign-in.html', password2_error=False, name=True)
+            else:
+                flash("Failed to connect to the database.")
+                return render_template('sign-in.html', password2_error=False, name=True)
         else:
             if password != password2:
                 return render_template('sign-in.html', password2_error=True, name= False)
@@ -41,8 +133,12 @@ def signin():
     return render_template('sign-in.html')
 
 
-
-
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    if "username" in session:
+        return render_template('home.html')
+    else:
+        return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
